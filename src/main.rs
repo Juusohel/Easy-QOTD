6aa394119d565a76112fd66d78948548ae9ce597
Juusoh <46687425+Juusohel@users.noreply.github.com>
2022-04-25 03:38:42 +0300

    Added dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::sync::Arc;
+use std::{env, fs};
+
+use serenity::framework::standard::{
+    macros::{command, group},
+    CommandResult, StandardFramework,
+};
+
+use serenity::utils::Color;
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+use tokio_postgres::NoTls;
+
 fn main() {
     println!("Hello, world!");
 }
