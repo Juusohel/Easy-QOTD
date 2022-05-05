cd8b6017be4d3b3aac7032abc9950f03b842034a
Juusoh <46687425+Juusohel@users.noreply.github.com>
2022-05-05 23:23:54 +0300

    Added database setup sql script

---
+++ qotd_database_setup.sql
@@ -0,0 +1,21 @@
+CREATE TABLE channels (
+guild_id varchar PRIMARY KEY,
+channel_id varchar NOT NULL
+);
+
+CREATE TABLE questions (
+question_id serial PRIMARY KEY,
+question_string varchar NOT NULL,
+in_use bool NOT NULL
+);
+
+CREATE TABLE custom_questions (
+question_id serial PRIMARY KEY,
+guild_id varchar NOT NULL,
+question_string varchar NOT NULL
+);
+
+CREATE TABLE ping_roles (
+guild_id varchar PRIMARY KEY,
+ping_role varchar NOT NULL
+);
