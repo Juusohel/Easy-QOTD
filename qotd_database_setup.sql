d8bb9f3f776bf73487dd279f8909ce3f29af9ab3
Juusoh <46687425+Juusohel@users.noreply.github.com>
2022-05-10 05:53:24 +0300

    Added database config changes to accommodate reaction polls

--- qotd_database_setup.sql
+++ qotd_database_setup.sql
@@ -4,18 +4,30 @@ channel_id varchar NOT NULL
 );
 
 CREATE TABLE questions (
-question_id serial PRIMARY KEY,
-question_string varchar NOT NULL,
-in_use bool NOT NULL
+    question_id serial PRIMARY KEY,
+    question_string varchar NOT NULL,
+    in_use bool NOT NULL
 );
 
 CREATE TABLE custom_questions (
-question_id serial PRIMARY KEY,
-guild_id varchar NOT NULL,
-question_string varchar NOT NULL
+    question_id serial PRIMARY KEY,
+    guild_id varchar NOT NULL,
+    question_string varchar NOT NULL
 );
 
 CREATE TABLE ping_roles (
-guild_id varchar PRIMARY KEY,
-ping_role varchar NOT NULL
+    guild_id varchar PRIMARY KEY,
+    ping_role varchar NOT NULL
+);
+
+CREATE TABLE polls (
+    poll_id serial PRIMARY KEY,
+    poll_string varchar NOT NULL,
+    in_use bool NOT NULL
+);
+
+CREATE TABLE custom_polls (
+    poll_id serial PRIMARY KEY,
+    guild_id varchar NOT NULL,
+    poll_string varchar NOT NULL
 );
